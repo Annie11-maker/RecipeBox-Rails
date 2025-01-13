107a9448823fce52e677d89e8287296c3670c7a8
quratulain shouket <quratulainshouket11@gmail.com>
2025-01-13 11:39:17 +0000

    cookbook seed done

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,6 @@
 class Category < ApplicationRecord
+  has_many :bookmarks, dependent: :destroy
+  has_many :recipes, through: :bookmarks
+
+  velidates :name, uniqueness: true, presence: true
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,7 @@
 class Recipe < ApplicationRecord
+  has_many :bookmarks
+
+  velidates :name, uniqueness: true, presence: true
+  velidates :description, presence: true
+  velidates :rating, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
 end
